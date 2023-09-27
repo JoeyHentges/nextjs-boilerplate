d79a1c68832551f9a5b611a4224af04401a6591b
jophen <joehentgesbusiness@gmail.com>
2023-09-26 22:35:43 -0500

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Acme",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  url: "https://acme.io",
+  ogImage: "https://acme.io/og.jpg",
+  links: {
+    twitter: "https://twitter.com/acme",
+    github: "https://github.com/acme",
+  },
+}
